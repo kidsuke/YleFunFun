8c2ba5b646a06aee53d533ac314629f737ed4a0f
Long Vu <longvh96@gmail.com>
2017-07-20 01:51:16 +0300

    Project init

---
+++ Assets/ScrollList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollList : MonoBehaviour {
+    public List<Item> items;
+    public GameObjectPool pool;
+    public Transform content;
+	// Use this for initialization
+	void Start () {
+        RefreshDisplay();
+	}
+
+    private void RefreshDisplay()
+    {
+        RemoveButtons();
+        PopulateButtons();
+    }
+
+    private void PopulateButtons() 
+    {
+        foreach (Item item in items) {
+            GameObject gameObject = pool.GetGameObject();
+            gameObject.transform.SetParent(content);
+
+            SuggestButton suggestButton = gameObject.GetComponent<SuggestButton>();
+            suggestButton.Bind(item);
+        }
+    }
+
+    private void RemoveButtons()
+    {
+        while (content.childCount > 0)
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            pool.PutToStack(toRemove);
+        }
+    }
+}
---
+++ Assets/SuggestButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuggestButton : MonoBehaviour {
+    public Text title;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Bind(Item item)
+    {
+        title.text = item.title;
+    }
+}
