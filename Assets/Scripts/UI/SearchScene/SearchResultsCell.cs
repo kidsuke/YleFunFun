1bf9967c62ae0773449d3992d65930790160473e
Long Vu <longvh96@gmail.com>
2017-07-26 19:29:01 +0300

    Restructure folder

---
+++ Assets/Scripts/UI/SearchScene/SearchResultsCell.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class SearchResultsCell : MonoBehaviour 
+{
+	public Text text;
+	public Button button;
+
+	private Item m_Item;
+
+	void Start () {
+		if (text == null) {
+			throw new Exception(this.GetType().Name + ": UI Text not found");
+		}
+		if (button == null) {
+			throw new Exception(this.GetType().Name + ": UI Button not found");
+		}
+
+		SearchSceneController controller = GameObject.FindObjectOfType<SearchSceneController>();
+		if (controller) {
+			button.onClick.AddListener(() => { print ("Hello"); controller.HandleOnItemClickedEvent(m_Item);});
+		}
+	}
+
+	void ScrollCellContent (Item item) 
+	{
+		m_Item = item;
+
+		if (text != null) 
+		{
+			text.text = item.title;
+		}
+	}
+}
