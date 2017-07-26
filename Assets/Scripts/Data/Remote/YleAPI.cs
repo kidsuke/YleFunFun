791976f8110adf4c72601e3ba44d155b8a7fb5e6
Long Vu <longvh96@gmail.com>
2017-07-26 23:51:49 +0300

    Create UrlBuilder to simplify url creation

--- Assets/Scripts/Data/Remote/YleAPI.cs
+++ Assets/Scripts/Data/Remote/YleAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -8,16 +9,19 @@ public class YleAPI {
 	public static readonly string BASE_URL = "https://external.api.yle.fi";
 	public static readonly string BASE_IMAGE_URL = "http://images.cdn.yle.fi/image/upload";
 	public static readonly string VERSION = "v1";
-	private string mAppId = "";
-	private string mAppKey = "";
+	private UrlBuilder m_Builder = new UrlBuilder(BASE_URL);
+	private string m_AppId = "d14e023d";
+	private string m_AppKey = "96da296c91609b7835e8bb861a4ca6fc";
 
 	//GET Request
 	public IEnumerator GetPrograms(IObserver<YleResponse> observer, CancellationToken cancellationToken, string query, int limit = 30, int offset = 0) {
 		using (
 			UnityWebRequest webRequest = UnityWebRequest.Get(
-				BASE_URL + "/" + VERSION + "/programs/items.json?" + 
-				GetAppId() + "&" + GetAppKey() + "&" +
-				"availability=ondemand" + "&" + "limit=" + limit + "&" + "offset=" + offset
+				UrlBuilder.Create(BASE_URL)
+				.Path(VERSION).Path("programs").Path("items.json")
+				.Query("availability", "ondemand").Query("limit", limit.ToString()).Query("offset", offset.ToString())
+				.Query("app_id", m_AppId).Query("app_key", m_AppKey)
+				.Build()
 			)
 		) {
 			yield return webRequest.Send();
@@ -26,7 +30,9 @@ public class YleAPI {
 				observer.OnError(new System.Exception(webRequest.error));
 			} else {
 				YleResponse response = JsonUtility.FromJson<YleResponse>(webRequest.downloadHandler.text);
-				observer.OnNext(response);
+				if (response != null) {
+					observer.OnNext(response);
+				}
 				observer.OnCompleted();
 			}
 		}
@@ -35,28 +41,66 @@ public class YleAPI {
 	public IEnumerator GetCoverImage(IObserver<Texture> observer, CancellationToken cancellationToken,  string id, string transformation = "",string format = "png") {
 		using (
 			UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(
-				BASE_IMAGE_URL + "/" + transformation + (transformation.Length == 0 ? "" : "/") + id + "." + format
+				UrlBuilder.Create(BASE_IMAGE_URL)
+				.Path(transformation).Path(id + "." + format)
+				.Build()
 			)
 		) {
-			Debug.Log(BASE_IMAGE_URL + "/" + transformation + (transformation.Length == 0 ? "" : "/") + id + "." + format);
 			yield return webRequest.Send();
 
 			if (webRequest.isHttpError || webRequest.isNetworkError) {
 				observer.OnError(new System.Exception(webRequest.error));
 			} else {
 				Texture texture = ((DownloadHandlerTexture) webRequest.downloadHandler).texture;
-				observer.OnNext(texture);
+				if (texture != null) {
+					observer.OnNext(texture);
+				}
 				observer.OnCompleted();
 			}
 		}
 	}
 
-	public string GetAppId()  {
-		return "app_id=" + mAppId;
-	}
+	public class UrlBuilder {
+		private StringBuilder m_Builder = new StringBuilder();
+		private bool m_BeginQuery = false;
 
-	public string GetAppKey()  {
-		return "app_key=" + mAppKey;
+		public UrlBuilder (string url) {
+			if (!url.EndsWith("/")) {
+				url.Remove(url.Length - 1);
+			}
+			m_Builder.Append(url);
+		}
+
+		public static UrlBuilder Create(string url) {
+			return new UrlBuilder(url);
+		}
+
+		public UrlBuilder Path (string path) {
+			if (path.Length != 0) {
+				m_Builder.Append("/").Append(path);
+			} else {
+				Debug.LogWarning(this.GetType().Name + ": Invalid path");
+			}
+			return this;
+		}
+
+		public UrlBuilder Query (string key, string value) {
+			if (key.Length !=0 && value.Length !=0) {
+				if (!m_BeginQuery) {
+					m_Builder.Append("?").Append(key).Append("=").Append(value);
+					m_BeginQuery = true;
+				} else {
+					m_Builder.Append("&").Append(key).Append("=").Append(value);
+				}
+			} else {
+				Debug.LogWarning(this.GetType().Name + ": Invalid query");
+			}
+			return this;
+		}
+
+		public string Build() {
+			return m_Builder.ToString();
+		}
 	}
-		
 }
+
