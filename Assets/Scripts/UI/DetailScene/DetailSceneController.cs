f1f1ba549efc7fc7b396f4becd3e30783530917a
Long Vu <longvh96@gmail.com>
2017-07-27 22:02:52 +0300

    Show error image if cannot load image from server

--- Assets/Scripts/UI/DetailScene/DetailSceneController.cs
+++ Assets/Scripts/UI/DetailScene/DetailSceneController.cs
@@ -9,6 +9,9 @@ public class DetailSceneController : MonoBehaviour {
 	private Image m_CoverImage;
 	public Image coverImage { get { return m_CoverImage; } set { m_CoverImage = value; } }
 	[SerializeField]
+	private Sprite m_DefaultSprite;
+	public Sprite defaultSprite { get { return m_DefaultSprite; } set { m_DefaultSprite = value; } }
+	[SerializeField]
 	private Text m_Title;
 	public Text tile { get { return m_Title; } set { m_Title = value; } }
 	[SerializeField]
@@ -30,13 +33,27 @@ public class DetailSceneController : MonoBehaviour {
 		m_Description.text = m_Item.description;
 	}
 
-	void GetCoverImage() {
+	public void GetCoverImage() {
 		string imageId = m_Item.imageId;
 		Observable.FromCoroutine<Texture>((observer, cancellationToken) => m_API.GetCoverImage(observer, cancellationToken, imageId))
-				  .Subscribe(texture => {
-					  Texture2D texture2D = texture as Texture2D;
-					  Sprite coverSprite = Sprite.Create(texture2D, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-					  m_CoverImage.sprite = coverSprite;
-				  });
+				  .Subscribe(
+						texture => {
+							if (texture != null) {
+								Texture2D texture2D = texture as Texture2D;
+								Sprite coverSprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f));
+								m_CoverImage.sprite = coverSprite;
+							} else {
+								loadDefaultErrorImage();
+							}
+						}, 
+						error => {
+							loadDefaultErrorImage();
+						});
+	}
+
+	private void loadDefaultErrorImage() {
+		m_CoverImage.sprite = defaultSprite;
+		m_CoverImage.type = Image.Type.Simple;
+		m_CoverImage.preserveAspect = true;
 	}
 }
